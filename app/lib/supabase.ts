8efbd5eebae7b609bee2b39b6f7778df228617c1
Yamazaki Shota <Yamazaki Shota>
2025-02-13 00:55:47 +0900

    hide env

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // SupabaseのURLとAPIキーを環境変数から読み込む
-const SUPABASE_URL = "https://pjvxcneopejcxwzoxkdq.supabase.co"
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqdnhjbmVvcGVqY3h3em94a2RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NzExNDEsImV4cCI6MjA1NDQ0NzE0MX0.wPjByO-1igsrY1cBhKlz7_lWwlWBnhA9Rhwwyl-asw0"
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
 
 // Supabaseクライアントを作成
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
